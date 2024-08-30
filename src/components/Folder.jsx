9fd4f63a108a1e0c01b7f4442f80e3d01124ab1a
Ваше Имя <_@example.com>
2024-08-30 11:23:50 +0600

    fix: fix folder component

---
+++ src/components/Folder.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+function Folder() {
+    return (
+        <div className="Folder">
+            <h2>test text test text</h2>
+        </div>
+    );
+}
+
+export default App;
